9cd92c46a492012b475ef9084d5a745423dbd0b2
0pippi0 <j.romany91@gmail.com>
2020-02-19 05:41:06 +0900

    Updates

---
+++ precache-manifest.45cba5cd45a92d22ae683988f741f7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a98f1ffeb443a9213cf61ec318d69f",
+    "url": "/github.io/practice_react_ref2/index.html"
+  },
+  {
+    "revision": "96d2b9fb067b23dce810",
+    "url": "/github.io/practice_react_ref2/static/css/main.1ee8c3e5.chunk.css"
+  },
+  {
+    "revision": "8d90cbd1387814ac94e8",
+    "url": "/github.io/practice_react_ref2/static/js/2.ae152c8d.chunk.js"
+  },
+  {
+    "revision": "2a6f7b0ac760b5d15fb8b482e9b9b618",
+    "url": "/github.io/practice_react_ref2/static/js/2.ae152c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d2b9fb067b23dce810",
+    "url": "/github.io/practice_react_ref2/static/js/main.4b3a497a.chunk.js"
+  },
+  {
+    "revision": "36d6c9c66a527c19e1d6",
+    "url": "/github.io/practice_react_ref2/static/js/runtime-main.458a58a9.js"
+  }
+]);
